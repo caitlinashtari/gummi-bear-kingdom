a8ae8ca62edd130cd3e78a93f1397720989eecc4
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-02-10 09:12:06 -0800

    add two more integration specs for testing edit functionality.

---
+++ spec/features/edit_product_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "the edit a product process" do
+  it "edits a product" do
+    product = Product.create(:name => 'Happy Bear', :cost => 13, :country => "FR", :interests => "Sky diving")
+    visit product_path(product)
+    click_on 'Edit'
+    fill_in 'Name', :with => 'Sad Bear'
+    click_on 'Update Product'
+    expect(page).to have_content 'Sad Bear'
+  end
+
+  it "gives error when no name is entered" do
+    product = Product.create(:name => 'Happy Bear', :cost => 13, :country => "FR", :interests => "Sky diving")
+    visit product_path(product)
+    click_on 'Edit'
+    fill_in 'Name', :with => ''
+    click_button 'Update Product'
+    expect(page).to have_content 'errors'
+  end
+end
