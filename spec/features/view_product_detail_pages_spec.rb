15657fb363f2d4011381f5338aa6d9b828245916
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-02-10 08:55:06 -0800

    add functionality to add a new product with partial form and errors.

---
+++ spec/features/view_product_detail_pages_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+# 
+# describe "the view product details process" do
+#   it "shows product details" do
+#
+#     visit products_path
+#     click_link 'Sunshine Bear'
+#     expect(page).to have_content 'Sunshine Bear'
+#   end
+# end
